6a9c521daab8a5028aed4babde6e1f54df1fbd18
Rinaldito <rinalditoryanari@gmail.com>
2022-05-03 22:39:38 +0700

    benerin layout comment

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,6 +5,10 @@
         </h2>
     </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-2">
         <div class="p-6 bg-white border-b border-gray-200">
             <span class="border-b text-blue-600 font-bold text-xl">
@@ -63,5 +67,7 @@
             </div>
         </div>
     @endforelse 
+
+    </div></div></div>
 
 </x-app-layout>
