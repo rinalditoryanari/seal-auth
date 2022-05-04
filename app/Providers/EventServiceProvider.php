f7205b4f0ba6b9e90f0dc6585c1167a4176606f2
Rinaldito <rinalditoryanari@gmail.com>
2022-05-04 08:08:50 +0700

    cuman benerin dikit

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -24,7 +24,6 @@ class EventServiceProvider extends ServiceProvider
         PostCommented::class => [
             PostCommentedMailListener::class,
             PostCommentedLogListener::class,
-
         ],
     ];
 
